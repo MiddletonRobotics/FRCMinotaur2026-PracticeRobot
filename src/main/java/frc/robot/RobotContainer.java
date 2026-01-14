a738a61b296d956161aa2f4a772a4a38a28dec49
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-01-13 21:27:40 -0500

    added drivetrain and needed button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,80 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.commands.DrivetrainCommands;
+import frc.robot.constants.DrivetrainConstants;
+import frc.robot.constants.GlobalConstants;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.GyroIO;
+import frc.robot.subsystems.drivetrain.GyroIOHardware;
+import frc.robot.subsystems.drivetrain.ModuleIO;
+import frc.robot.subsystems.drivetrain.ModuleIOHardware;
 
 public class RobotContainer {
+  private final Drivetrain drivetrain;
+  private final CommandXboxController primaryController = new CommandXboxController(0);
+  private final LoggedDashboardChooser<Command> autonomousChooser;
+
   public RobotContainer() {
+    switch (GlobalConstants.kCurrentMode) {
+      case REAL:
+        drivetrain = new Drivetrain(
+          new GyroIOHardware(), 
+          new ModuleIOHardware(0, DrivetrainConstants.kFrontLeftModuleConstants), 
+          new ModuleIOHardware(1, DrivetrainConstants.kFrontRightModuleConstants), 
+          new ModuleIOHardware(2, DrivetrainConstants.kBackLeftModuleConstants),
+          new ModuleIOHardware(3, DrivetrainConstants.kBackRightModuleConstants)
+        );
+
+        break;
+      case SIM:
+        drivetrain = new Drivetrain(new GyroIO() {}, new ModuleIO() {}, new ModuleIO() {}, new ModuleIO() {}, new ModuleIO() {});
+        break;
+      default:
+        drivetrain = new Drivetrain(new GyroIO() {}, new ModuleIO() {}, new ModuleIO() {}, new ModuleIO() {}, new ModuleIO() {});
+        break;
+    }
+
+    autonomousChooser = new LoggedDashboardChooser<>("Auton Choices", AutoBuilder.buildAutoChooser());
+    autonomousChooser.addOption("Drivetrain Wheel Radius Characterization", DrivetrainCommands.wheelRadiusCharacterization(drivetrain));
+    autonomousChooser.addOption("Drivetrain Simple FF Characterization", DrivetrainCommands.feedforwardCharacterization(drivetrain));
+    autonomousChooser.addOption("Drivetrain SysId (Quasistatic Forward)", drivetrain.sysIdQuasistatic(Direction.kForward));
+    autonomousChooser.addOption("Drivetrain SysId (Quasistatic Reverse)", drivetrain.sysIdQuasistatic(Direction.kReverse));
+    autonomousChooser.addOption("Drivetrain SysId (Dynamic Forward)", drivetrain.sysIdDynamic(Direction.kForward));
+    autonomousChooser.addOption("Drivetrain SysId (Dynamic Forward)", drivetrain.sysIdDynamic(Direction.kReverse));
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    drivetrain.setDefaultCommand(DrivetrainCommands.joystickDrive(
+      drivetrain, 
+      () -> -primaryController.getLeftY(), 
+      () -> -primaryController.getLeftX(), 
+      () -> -primaryController.getRightX()
+    ));
+
+    primaryController.a().whileTrue(DrivetrainCommands.joystickDriveAtAngle(
+      drivetrain,
+      () -> -primaryController.getLeftY(),
+      () -> -primaryController.getLeftX(),
+      () -> Rotation2d.kZero
+    ));
+
+    primaryController.x().onTrue(Commands.runOnce(drivetrain::stopWithX, drivetrain));
+    primaryController.b().onTrue(Commands.runOnce(() ->
+      drivetrain.setPose(new Pose2d(drivetrain.getPose().getTranslation(), Rotation2d.kZero)), drivetrain).ignoringDisable(true));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
